b63461940b64ad175957832f37e937066cd0c51e
jrglo <jrg.lopez07@gmail.com>
2021-03-25 20:17:54 -0700

    Se carga proyecto

---
+++ src/labs/pm/app/Shop.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (C) 2021 jrglo
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import labs.pm.data.Product;
+import labs.pm.data.ProductManager;
+import labs.pm.data.Rating;
+
+/**
+ * {@code Shop} class represents an application that manages products
+ * @version 4.0	
+ * @author Jorge Augusto Lopez Ortega
+ */
+public class Shop {
+	 
+	/**
+	 * @param args the comand line arguments 
+	 */
+	public static void main(String[] args) {
+		ProductManager pm = new ProductManager();
+		
+		Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
+		Product p2 = pm.createProduct(102, "Coffe", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+		Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(1.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+		Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now().plusDays(2));
+		Product p5 = p3.applyRating(Rating.THREE_STAR);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		System.out.println(p4);
+		System.out.println(p5);
+		
+		Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
+		Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+		System.out.println(p6);
+		System.out.println(p7);
+		
+		Product p8 = p4.applyRating(Rating.FIVE_STAR);
+		Product p9 = p1.applyRating(Rating.TWO_STAR);
+		System.out.println(p8);
+		System.out.println(p9);
+		
+	}
+
+}
